44cbe82d7b3226bfc6f4ff38df08c3b7262ab04d
Simon Pena <spena@igalia.com>
2012-03-08 13:11:30 +0100

    Added copyright notice
    Release the plugin as LGPL

--- gallerytiltshiftplugin/gallerytiltshiftplugin.cpp
+++ gallerytiltshiftplugin/gallerytiltshiftplugin.cpp
@@ -1,3 +1,25 @@
+/*
+ * gallerytiltshiftplugin.cpp
+ *
+ * Copyright (C) 2012 Igalia, S.L.
+ * Author: Simon Pena <spena@igalia.com>
+ *
+ * This file is part of the Gallery Tilt Shift Plugin.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see http://www.gnu.org/licenses/ *
+ */
+
 #include "gallerytiltshiftplugin.h"
 #include "gallerytiltshiftplugin_p.h"
 #include "gallerytiltshiftwidget.h"
